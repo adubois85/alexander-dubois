8154349f4fc46fb08693dc0742865e19a30584ba
Alexander DuBois <adubois@alumni.uci.edu>
2017-07-20 00:13:33 -0600

    Purpose, audience, goal added

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -5,6 +5,25 @@
 		<title>Alexander DuBois' Personal Portfolio</title>
 	</head>
 	<body>
-		<p>Stuff here</p>
+		<p>
+			<strong>Purpose:</strong> This website will serve as a personal portfolio of my work.  I will use it to
+			display relevant professional information about myself as well as links to my code bases and completed
+			projects that I have worked on.
+		</p>
+
+		<p>
+			<strong>Audience:</strong> The primary group of people I will be building my personal webpage towards are
+			developers and engineers who hire for their companies.  The page will serve as an at-a-glance example of my
+			techinical abilities, with links to futher projects should their first impressions prove favorable.
+		</p>
+
+		<p>
+			<strong>Goal:</strong> The primary goal of this page will be to serve as a launchpad for paid work
+			opportunities, ideally full-time with a company that I am interested in.  Aside from that, it will also
+			serve as a example of my work for any employers looking for potential freelancers.
+		</p>
+
+		<h1>Persona</h1>
+
 	</body>
 </html>
