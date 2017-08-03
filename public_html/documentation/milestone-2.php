945d4caf5a0b09f22777c0539a20d789e8e10e19
Alexander DuBois <adubois@alumni.uci.edu>
2017-08-03 04:17:36 -0600

    Added milestone 2, wireframe images, content strategy

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Alexander DuBois' Personal Portfolio</title>
+	</head>
+	<body>
+		<h1>Content Strategy</h1>
+		<p>
+			This will be a personal portfolio website.  The site will be broken up into four main sections and accessed
+			through a central hub.  Clicking on one of the arrows will move that arrow to the side of the screen and
+			cause a frame to slide in from the other side with the relevant content.  The four sections will be a
+			bio--information about myself as well as links to my github and social media accounts, a resumeé--my current
+			resumé, projects--a showcase of projects I am or have worked on, and contact--a secure contact submission form.
+		</p>
+
+		<h1>Wireframes</h1>
+		<span><img src="/images/wireframe-mobile.png" alt="Mobile Wireframe" /></span>
+		<span><img src="/images/wireframe-desktop.png" alt="Desktop Wireframe" /></span>
+
+	</body>
+</html>
