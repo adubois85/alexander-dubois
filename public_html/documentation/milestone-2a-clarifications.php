5606c505876be1e7525c115ed7164da9180fda0c
Alexander DuBois <adubois@alumni.uci.edu>
2017-08-07 23:43:48 -0600

    added clarifications page, first clarification outlined

---
+++ public_html/documentation/milestone-2a-clarifications.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Clarifications on Milestone 2a</title>
+	</head>
+	<body>
+		<p>
+			So, how I had originally envisioned the website was that the page would open on a small welcome message with the clickable arrows around it.  Clicking on each would slide that section into view from the opposite side of the arrow.
+		</p>
+		<ol>
+			<li>
+				Regarding the difference between the mobile and desktop versions, it was during wireframming that it became apparent how much more empty space there would be in the desktop design.  Since I think it would look unsightly to try and actually use all that space (key word try--it would still largely be empty, just inside of a giant frame covering most of the browser window), I decided that I would be better served having a more proportionate frame to contain each section slide into view.  Also, due to how much more quickly a phone or other small device's screen can become visually cluttered, I figured it would make more sense to have the frames be opaque.  In contrast, a desktop browser has much more breathing room, so having some relatively small and superfluous visual flairs (e.g. a semi-transparent frame for the different sections) can add a bit of aesthetic interest without significantly detracting from the experience.
+			</li>
+		</ol>
+
+	</body>
+</html>
