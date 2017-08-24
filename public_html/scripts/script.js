c4f78fd64fab64ac8db548c0f99a78ab8288dba9
Alexander DuBois <adubois@alumni.uci.edu>
2017-08-23 18:02:45 -0600

    removed unused functions/comments, added semicolons

--- public_html/scripts/script.js
+++ public_html/scripts/script.js
@@ -1,26 +1,16 @@
-
-//			function toggleTop() {
-//				document.getElementsByClassName("section-main");
-//			}
-
-
-function alertTest() {
-	alert("hello");
-}
-
 $(".fa-caret-up").on("click", function() {
 	$(".section-about-me").toggle(2000);
-})
+});
 
 $(".fa-caret-right").on("click", function() {
 	$(".section-projects").toggle(2000);
-})
+});
 
 $(".fa-caret-down").on("click", function() {
 	$(".section-contact-form").toggle(2000);
-})
+});
 
 $(".fa-caret-left").on("click", function() {
 	$(".section-resume").toggle(2000);
-})
+});
 
