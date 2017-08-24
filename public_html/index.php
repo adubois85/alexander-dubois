2cc9843cb9dbca238ef3648ecc7d6d80d15eff17
Alexander DuBois <adubois@alumni.uci.edu>
2017-08-23 18:01:54 -0600

    properly linked jquery now, created functions to show each section

--- public_html/index.php
+++ public_html/index.php
@@ -35,29 +35,13 @@
 <!--			</div>-->
 <!--		</section>-->
 
-		<script>
-//			function toggleTop() {
-//				document.getElementsByClassName("section-main");
-//			}
-
-
-			function toggleTop() {
-				alert("hello");
-			}
-			function a(){
-				this.classList.toggle('hidden');}
-				document.querySelector('#container').addEventListener('mouseclick', a );
-			}
-		</script>
-
-
 				<section class="section-main">
 					<div class="container">
 						<div class="row justify-content-center p-0">
-							<button type="button" class="btn btn-link" id="up-button">
-								<i class="fa fa-caret-up align-self-start border pt-0" onclick="toggleTop()"
-									aria-hidden="true"></i>
-							</button>
+<!--<!--							<button type="button" class="btn btn-link" id="up-button">-->
+<!--<!--								<i class="fa fa-caret-up align-self-start border pt-0" onclick="alertTest()"-->
+<!--<!--									aria-hidden="true"></i>-->
+<!--							</button>-->
 							<a class="btn btn-default border fa fa-caret-up d-inline" href="#"></a>
 						</div>
 					</div>
@@ -126,12 +110,13 @@
 			<button class="btn btn-primary" type="submit">Submit</button>
 		</section>
 
-		<!-- Optional JavaScript -->
-		<script src="scripts/script.js"></script>
+
 
 		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+		<!-- My JavaScript file -->
+		<script src="scripts/script.js"></script>
 	</body>
 </html>
