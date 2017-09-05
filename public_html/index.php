ffe21cb704a93580e8be943fec78ca99d0b13f4e
Alexander DuBois <adubois@alumni.uci.edu>
2017-09-05 00:18:02 -0600

    Updated all popout sections to modal dialogs

--- public_html/index.php
+++ public_html/index.php
@@ -55,25 +55,29 @@
 
 		<!-- This section is a showcase of other projects I've worked on -->
 		<section class="section-projects">
-			<div class="container-fluid" id="modal-projects">
-				<div class="row project1">
-					<a href="#"><img src="https://www.fillmurray.com/300/300" alt="foodquisition" class="img-fluid float-left
-					rounded col-xs-3" /></a>
-					<h5>Foodquisition</h5>
-					<p>
-						Have you ever wondered how well the restaurant you're eating at scored on its health inspection?
-						Well, wonder no more.  A group project built primarily in MySQL, PHP, Angular, and Javascript with
-						3 other students
-					</p>
-				</div>
-				<div class="row project2">
-					<a href="#"><img src="https://www.fillmurray.com/g/300/300" alt="placeholder" class="img-fluid
-					rounded"/></a>
-					<h5>Project 2</h5>
-					<p>
-						This is a description of the second project in this gallery.  It talks about what the project's aims
-						are, what language(s) it was built with, and maybe a witty blurb.
-					</p>
+			<div class="modal fade" id="modal-projects" tabindex="-1" role="dialog" aria-labelledby="Other Projects">
+				<div class="modal-dialog modal-lg" role="document">
+					<div class="modal-content">
+						<div class="row project1">
+							<a href="#"><img src="https://www.fillmurray.com/300/300" alt="foodquisition" class="img-fluid float-left
+						rounded col-xs-3" /></a>
+							<h5>Foodquisition</h5>
+							<p>
+								Have you ever wondered how well the restaurant you're eating at scored on its health inspection?
+								Well, wonder no more.  A group project built primarily in MySQL, PHP, Angular, and Javascript with
+								3 other students
+							</p>
+						</div>
+						<div class="row project2">
+							<a href="#"><img src="https://www.fillmurray.com/g/300/300" alt="placeholder" class="img-fluid
+						rounded"/></a>
+							<h5>Project 2</h5>
+							<p>
+								This is a description of the second project in this gallery.  It talks about what the project's aims
+								are, what language(s) it was built with, and maybe a witty blurb.
+							</p>
+						</div>
+					</div>
 				</div>
 			</div>
 		</section>
@@ -81,116 +85,125 @@
 
 		<!-- This section is for my resumé -->
 		<section class="section-resume">
-			<div class="container" id="modal-resume">
-				<div class="resume-title">
-					<div class="row justify-content-center">
-						<p class="col-xs-12"><em>Alexander DuBois</em></p>
-					</div>
-					<div class="row justify-content-center">
-						<p class="col-xs-12"><em>Albuquerque, New Mexico 87114</em> -- Willing to
-							relocate</p>
-					</div>
-					<div class="row justify-content-center">
-						<p class="col-xs-12 col-md-3"><em>(760) 793-3972</em></p>
-						<p class="col-xs-12 col-md-3"><em>adubois@alumni.uci.edu</em></p>
-					</div>
-					<div class="row justify-content-center">
-						<p class="col-xs-12 col-md-3">GitHub--<em>adubois85</em></p>
-						<p class="col-xs-12 col-md-3">LinkedIn--<em>/in/dubious</em></p>
-					</div>
-				</div>
+			<div class="modal fade" id="modal-resume" tabindex="-1" role="dialog" aria-labelledby="Resumé">
+				<div class="modal-dialog modal-lg" role="document">
+					<div class="modal-content">
 
-				<div class="resume-skills">
-					<div class="row">
-						<p class="col-xs-12 col-md-4"><em>Languages</em></p>
-						<p class="col-xs-12 col-md-8">PHP, Javascript, MySQL, HTML5, CSS</p>
-					</div>
-					<div class="row">
-						<p class="col-xs-12 col-md-4"><em>Tools</em></p>
-						<p class="col-xs-12 col-md-8">GitHub, Bootstrap, AngularJS, PHPStorm, Atom</p>
-					</div>
-					<div class="row">
-						<p class="col-xs-12 col-md-4"><em>Other Skills</em></p>
-						<p class="col-xs-12 col-md-8">50+ WPM typing speed, 4,000 KPH 10-key, Attentive to details and will excel individually or with a team</p>
-					</div>
-				</div>
+						<div class="resume-title">
+							<div class="row justify-content-center">
+								<p class="col-xs-12"><em>Alexander DuBois</em></p>
+							</div>
+							<div class="row justify-content-center">
+								<p class="col-xs-12"><em>Albuquerque, New Mexico 87114</em> -- Willing to
+									relocate</p>
+							</div>
+							<div class="row justify-content-center">
+								<p class="col-xs-12 col-md-3"><em>(760) 793-3972</em></p>
+								<p class="col-xs-12 col-md-3"><em>adubois@alumni.uci.edu</em></p>
+							</div>
+							<div class="row justify-content-center">
+								<p class="col-xs-12 col-md-3">GitHub--<em>adubois85</em></p>
+								<p class="col-xs-12 col-md-3">LinkedIn--<em>/in/dubious</em></p>
+							</div>
+						</div>
 
-				<div class="resume-experience">
-					<h6>Recent Experience</h6>
-					<div class="row justify-content-between">
-						<p class="col-xs-6"><em>Data Specialist</em> -- SL Start</p>
-						<p class="col-xs-6"><em>August, 2015 to April, 2016</em></p>
+						<div class="resume-skills">
+							<div class="row">
+								<p class="col-xs-12 col-md-4"><em>Languages</em></p>
+								<p class="col-xs-12 col-md-8">PHP, Javascript, MySQL, HTML5, CSS</p>
+							</div>
+							<div class="row">
+								<p class="col-xs-12 col-md-4"><em>Tools</em></p>
+								<p class="col-xs-12 col-md-8">GitHub, Bootstrap, AngularJS, PHPStorm, Atom</p>
+							</div>
+							<div class="row">
+								<p class="col-xs-12 col-md-4"><em>Other Skills</em></p>
+								<p class="col-xs-12 col-md-8">50+ WPM typing speed, 4,000 KPH 10-key, Attentive to details and will excel individually or with a team</p>
+							</div>
+						</div>
+
+						<div class="resume-experience">
+							<h6>Recent Experience</h6>
+							<div class="row justify-content-between">
+								<p class="col-xs-6"><em>Data Specialist</em> -- SL Start</p>
+								<p class="col-xs-6"><em>August, 2015 to April, 2016</em></p>
+							</div>
+							<ul>
+								<li>Screened, counseled and advised clients on necessary documentation and eligibility requirements for SNAP (food stamps)</li>
+								<li>
+									Conducted outreach to community based organizations, emergency food programs and other
+									partners, providing information on SNAP rules and regulations
+								</li>
+								<li>Provided facilitated enrollment services to clients to apply and re-certify for SNAP</li>
+								<li>Responsible for ensuring that all applications are complete with proper supporting documentation</li>
+								<li>Knowledgeable of benefits screening, especially for SNAP</li>
+							</ul>
+						</div>
+						<div class="temp-jobs">
+							<h6>Temp Jobs</h6>
+							<div class="row justify-content-between">
+								<p class="col-xs-6"><em>Various</em></p>
+								<p class="col-xs-6"><em>2011 - 2014</em></p>
+							</div>
+							<ul>
+								<li>Restaurants, Direct Care Staff Employee, Division of Vocational Rehabilitation</li>
+							</ul>
+						</div>
+						<div class="education">
+							<h6>Education</h6>
+							<div class="row justify-content-between">
+								<p class="col-xs-6"><em>B.A.</em>Psychology and Social Behavior</p>
+								<p class="col-xs-6"><em>September 2007</em></p>
+							</div>
+							<div class="row">
+								<p><em>B.A.</em>Studio Art</p>
+							</div>
+							<div class="row">
+								<p class="offset-xs-1">University of California, Irvine</p>
+							</div>
+						</div>
+						<div class="references">
+							<h6>References</h6>
+							<p>Available upon request</p>
+						</div>
 					</div>
-					<ul>
-						<li>Screened, counseled and advised clients on necessary documentation and eligibility requirements for SNAP (food stamps)</li>
-						<li>
-							Conducted outreach to community based organizations, emergency food programs and other
-							partners, providing information on SNAP rules and regulations
-						</li>
-						<li>Provided facilitated enrollment services to clients to apply and re-certify for SNAP</li>
-						<li>Responsible for ensuring that all applications are complete with proper supporting documentation</li>
-						<li>Knowledgeable of benefits screening, especially for SNAP</li>
-					</ul>
-				</div>
-				<div class="temp-jobs">
-					<h6>Temp Jobs</h6>
-					<div class="row justify-content-between">
-						<p class="col-xs-6"><em>Various</em></p>
-						<p class="col-xs-6"><em>2011 - 2014</em></p>
-					</div>
-					<ul>
-						<li>Restaurants, Direct Care Staff Employee, Division of Vocational Rehabilitation</li>
-					</ul>
-				</div>
-				<div class="education">
-					<h6>Education</h6>
-					<div class="row justify-content-between">
-						<p class="col-xs-6"><em>B.A.</em>Psychology and Social Behavior</p>
-						<p class="col-xs-6"><em>September 2007</em></p>
-					</div>
-					<div class="row">
-						<p><em>B.A.</em>Studio Art</p>
-					</div>
-					<div class="row">
-						<p class="offset-xs-1">University of California, Irvine</p>
-					</div>
-				</div>
-				<div class="references">
-					<h6>References</h6>
-					<p>Available upon request</p>
 				</div>
 			</div>
 		</section>
 
 		<!-- This section is a secured form for contacting me -->
 		<section class="section-contact-form">
-			<div class="container" id="modal-contact">
-				<label for="contact-name">Your name</label>
-				<div class="input-group input-group-lg">
-					<span class="input-group-addon" id="contact-name"><i class="fa fa-user" aria-hidden="true"></i></span>
-					<input type="text" class="form-control" placeholder="What should I call you?" aria-label="Your name" />
-				</div>
+			<div class="modal fade" id="modal-contact" tabindex="-1" role="dialog" aria-labelledby="Contact Form">
+				<div class="modal-dialog modal-lg" role="document">
+					<div class="modal-content">
+						<label for="contact-name">Your name</label>
+						<div class="input-group input-group-lg">
+							<span class="input-group-addon" id="contact-name"><i class="fa fa-user" aria-hidden="true"></i></span>
+							<input type="text" class="form-control" placeholder="What should I call you?" aria-label="Your name" />
+						</div>
 
-				<label for="contact-email">Your email</label>
-				<div class="input-group">
-					<span class="input-group-addon" id="contact-email"><i class="fa fa-envelope" aria-hidden="true"></i></span>
-					<input type="text" class="form-control" placeholder="Where can I reach you?" aria-label="Your email
-				address" />
-				</div>
+						<label for="contact-email">Your email</label>
+						<div class="input-group">
+							<span class="input-group-addon" id="contact-email"><i class="fa fa-envelope" aria-hidden="true"></i></span>
+							<input type="text" class="form-control" placeholder="Where can I reach you?" aria-label="Your email
+						address" />
+						</div>
 
-				<label for="contact-subject">Email subject</label>
-				<div class="input-group">
-					<span class="input-group-addon" id="contact-subject"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></span>
-					<input type="text" class="form-control" placeholder="Why are you contacting me today?" aria-label="Email
-				subject line" />
-				</div>
+						<label for="contact-subject">Email subject</label>
+						<div class="input-group">
+							<span class="input-group-addon" id="contact-subject"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></span>
+							<input type="text" class="form-control" placeholder="Why are you contacting me today?" aria-label="Email
+						subject line" />
+						</div>
 
-				<label for="contact-body">Email body</label>
-				<div class="input-group">
-				<textarea class="form-control"  id="contact-body" rows=6 placeholder="What's on your mind? (4000 characters max)" aria-label="Email body"></textarea>
-				</div>
+						<label for="contact-body">Email body</label>
+						<div class="input-group">
+						<textarea class="form-control"  id="contact-body" rows=6 placeholder="What's on your mind? (4000 characters max)" aria-label="Email body"></textarea>
+						</div>
 
-				<button class="btn btn-primary" type="submit">Submit</button>
+						<button class="btn btn-primary" type="submit">Submit</button>
+					</div>
+				</div>
 			</div>
 		</section>
 
